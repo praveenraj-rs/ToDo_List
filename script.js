d1f45606ec2e79dc30ff38a6da29c7173b2d2f79
praveenraj-rs <praveenravi515@gmail.com>
2023-09-30 23:10:40 +0530

    optimised the javascript and object to store the tasks for easy manipulation

--- script.js
+++ script.js
@@ -4,56 +4,66 @@ let tasks = document.querySelector(".tasks");
 let taskList = document.querySelector(".taskList");
 let delButton = document.querySelector(".delButton");
 
+let todo = [
+  {
+    id: 1,
+    value: "Task 1",
+    checked: false,
+  },
+  {
+    id: 2,
+    value: "Taks 2",
+    checked: true,
+  },
+  {
+    id: 3,
+    value: "Task 3",
+    checked: false,
+  },
+];
+
+// Creating Task Elements from todo Object
+function renderTask() {
+  tasks.innerHTML = "";
+  todo.forEach((list) => {
+    let div = document.createElement("div");
+    div.className = `taskList ${list.checked ? 'checked' : ''}`;
+    div.innerHTML = `<span onclick="toggleComplete(${list.id})">${list.value}<button onclick="removeTask(${list.id})">&#10005;</button>`;
+
+    tasks.append(div);
+  });
+}
+
+// addTask Function
+function addTask() {
+  let newTaskText = rowInput.value.trim();
+  if (newTaskText !== "") {
+    const newTask = { id: Date.now(), value: newTaskText, checked: false };
+    todo.push(newTask);
+    renderTask();
+    rowInput.value = "";
+  }
+}
+
+// removeTask Function
+function removeTask(taskId) {
+  todo = todo.filter((list) => list.id !== taskId);
+  renderTask();
+}
+
+function toggleComplete(taskId) {
+  todo = todo.map(task =>
+      task.id === taskId ? { ...task, checked: !task.checked } : task
+  );
+  renderTask();
+}
+
+
 // Clicking Enter to add the task to the list
 rowInput.addEventListener("keypress", (e) => {
   if (e.key == "Enter") {
-    append();
+    addTask();
   }
 });
 
-function append() {
-  // Getting the task data from input field
-  let task = rowInput.value;
-
-  // Checking the empty tasks
-  if (task == "") {
-    alert("Enter Some Value");
-  } else {
-    let div = document.createElement("div");
-    div.classList.add("taskList");
-    div.innerText = task;
-
-    let button = document.createElement("button");
-    button.innerHTML = "&#10005;";
-    button.setAttribute(
-      "onClick",
-      "this.parentElement.style.display='none';save()"
-    );
-    div.appendChild(button);
-
-    if (tasks.childNodes[0] == null) {
-      tasks.appendChild(div);
-    } else {
-      tasks.insertBefore(div, tasks.childNodes[0]);
-    }
-    document.querySelector(".inputRow input").value = "";
-
-    save();
-  }
-}
-
-function save() {
-  localStorage.setItem("data", document.querySelector(".tasks").innerHTML);
-}
-
-document.querySelector(".tasks").innerHTML = localStorage.getItem("data");
-
-var list = document.querySelector(".tasks");
-list.addEventListener("click", function (ev) {
-  if (ev.target.classList == "tasks") {
-  } else {
-    console.log(ev.target);
-    ev.target.classList.toggle("checked");
-    save();
-  }
-});
+renderTask();
