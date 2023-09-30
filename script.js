5bdb19a50b071609ea5b803c9d06e10e52d71908
praveenraj-rs <praveenravi515@gmail.com>
2023-09-30 17:57:28 +0530

    udated the task completion tick mark

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// "this.parentElement.style.display='none';save()"
+
 // DOM
 let rowInput = document.querySelector(".inputRow input");
 let tasks = document.querySelector(".tasks");
+let delButton = document.querySelector(".delButton");
+let taskList = document.querySelector(".taskList")
 
 // Clicking Enter to add the task to the list
 rowInput.addEventListener("keypress", (e) => {
@@ -10,7 +14,6 @@ rowInput.addEventListener("keypress", (e) => {
 });
 
 function append() {
-
   // Getting the task data from input field
   let task = rowInput.value;
 
@@ -22,17 +25,10 @@ function append() {
     div.classList.add("taskList");
     div.innerText = task;
 
-    let input = document.createElement("input");
-    input.classList.add("checkbx");
-    input.setAttribute("type", "checkbox");
-    div.appendChild(input);
 
     let button = document.createElement("button");
     button.innerHTML = "&#10005;";
-    button.setAttribute(
-      "onclick",
-      "this.parentElement.style.display='none';save()"
-    );
+    button.setAttribute("onClick","this.parentElement.style.display='none';save()")
     div.appendChild(button);
 
     tasks.appendChild(div);
@@ -40,10 +36,23 @@ function append() {
 
     save();
   }
-  }
+}
 
 function save() {
   localStorage.setItem("data", document.querySelector(".tasks").innerHTML);
 }
 
 document.querySelector(".tasks").innerHTML = localStorage.getItem("data");
+
+var list = document.querySelector('.tasks');
+list.addEventListener('click', function(ev) {
+  if(ev.target.classList=="tasks"){
+  
+  }
+  else{
+  console.log(ev.target)
+  ev.target.classList.toggle('checked');
+  save()
+  }
+}
+)
